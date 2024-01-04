c4fbd67e6a69a2284dfb592f73172542fa36a775
Stlcks <nathanccosta23@gmail.com>
2024-01-03 22:49:31 -0300

    test

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return <div>
+    <Link href="/">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolore sint ex voluptates minima sapiente modi adipisci illo architecto pariatur, dolorem eius, labore non! Rerum aperiam sapiente deserunt quis facere?
+      </h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat obcaecati, adipisci minima quibusdam sapiente iure necessitatibus quos. Quis reprehenderit enim, cum dolorum mollitia beatae quas velit aut facilis, iure nam!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat obcaecati, adipisci minima quibusdam sapiente iure necessitatibus quos. Quis reprehenderit enim, cum dolorum mollitia beatae quas velit aut facilis, iure nam!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat obcaecati, adipisci minima quibusdam sapiente iure necessitatibus quos. Quis reprehenderit enim, cum dolorum mollitia beatae quas velit aut facilis, iure nam!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat obcaecati, adipisci minima quibusdam sapiente iure necessitatibus quos. Quis reprehenderit enim, cum dolorum mollitia beatae quas velit aut facilis, iure nam!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat obcaecati, adipisci minima quibusdam sapiente iure necessitatibus quos. Quis reprehenderit enim, cum dolorum mollitia beatae quas velit aut facilis, iure nam!</Link>
+  </div>;
+}
