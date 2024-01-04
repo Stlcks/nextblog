82ca8d868f7c9831bb519df45322e035edbb90aa
Stlcks <nathanccosta23@gmail.com>
2024-01-03 22:51:02 -0300

    teste commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Blog App',
   description: 'The best blog app!',
-}
+} 
 
 export default function RootLayout({ children }) {
   return (
